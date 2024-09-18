ec15cfe47fb954d34c52d44ceb3cc5c4b9253e97
김지환 <91196025+Kimjipang@users.noreply.github.com>
2024-09-18 16:25:17 +0900

    initial commit

---
+++ src/main/java/yeogi/moim/YeogiMoimApplication.java
@@ -0,0 +1,13 @@
+package yeogi.moim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeogiMoimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeogiMoimApplication.class, args);
+	}
+
+}
