8f05797b5b187972941c229a640476890091094e
김지환 <91196025+Kimjipang@users.noreply.github.com>
2024-10-07 19:26:46 +0900

    #2 Style: 엔드포인트 복수형으로 수정

--- src/main/java/yeogi/moim/member/controller/MemberController.java
+++ src/main/java/yeogi/moim/member/controller/MemberController.java
@@ -4,34 +4,34 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import yeogi.moim.member.dto.MemberRequestDto;
-import yeogi.moim.member.dto.MemberResponseDto;
+import yeogi.moim.member.dto.MemberRequest;
+import yeogi.moim.member.dto.MemberResponse;
 import yeogi.moim.member.service.MemberService;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/member")
+@RequestMapping("/api/members")
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
     @PostMapping
-    public ResponseEntity<MemberResponseDto> registerMember(@RequestBody MemberRequestDto memberRequestDto) {
-        MemberResponseDto memberResponseDto = memberService.registerMember(memberRequestDto);
-        return new ResponseEntity<>(memberResponseDto, HttpStatus.CREATED);
+    public MemberResponse registerMember(@RequestBody MemberRequest memberRequest) {
+        MemberResponse memberResponse = memberService.registerMember(memberRequest);
+        return memberResponse;
     }
 
     @GetMapping
-    public ResponseEntity<List<MemberResponseDto>> getMemberList() {
-        List<MemberResponseDto> memberResponseDtoList = memberService.getMemberList();
-        return new ResponseEntity<>(memberResponseDtoList, HttpStatus.OK);
+    public ResponseEntity<List<MemberResponse>> getMemberList() {
+        List<MemberResponse> memberResponseList = memberService.getMemberList();
+        return new ResponseEntity<>(memberResponseList, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<MemberResponseDto> getMember(@PathVariable Long id) {
-        MemberResponseDto memberResponseDto = memberService.getMember(id);
-        return new ResponseEntity<>(memberResponseDto, HttpStatus.OK);
+    @GetMapping("/{memberId}")
+    public ResponseEntity<MemberResponse> getMember(@PathVariable Long memberId) {
+        MemberResponse memberResponse = memberService.getMember(memberId);
+        return new ResponseEntity<>(memberResponse, HttpStatus.OK);
     }
 }
