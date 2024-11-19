64d9b9c81d35affa79906a0db4c547aab9631560
김지환 <91196025+Kimjipang@users.noreply.github.com>
2024-11-20 01:56:06 +0900

    #6 Feat: 사용자 인가 로직 메서드로 분리

--- src/main/java/yeogi/moim/authentication/service/AuthenticationService.java
+++ src/main/java/yeogi/moim/authentication/service/AuthenticationService.java
@@ -1,5 +1,6 @@
 package yeogi.moim.authentication.service;
 
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -13,5 +14,13 @@ public class AuthenticationService {
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
 
         return userDetails.getMemberId();
+    }
+
+    public void authorizeMember(Long memberId) {
+        Long authenticatedMemberId = getAuthenticatedMemberId();
+
+        if (!authenticatedMemberId.equals(memberId)) {
+            throw new IllegalArgumentException("입력받은 정보가 인증된 사용자와 일치하지 않습니다.");
+        }
     }
 }
